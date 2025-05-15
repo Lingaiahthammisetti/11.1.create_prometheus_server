9ed749be5dbc5116b01c1e9562a9b071f79168d1
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-05-15 20:30:43 +0530

    prometheus installation

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-bash /root/11.1.create_prometheus_server/Installations/Prometheus/prometheus.sh
-bash /root/11.1.create_prometheus_server/Installations/Grafana/grafana.sh
-bash /root/11.1.create_prometheus_server/Installations/alert-manager/alertmanager.sh
