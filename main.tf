8660229356ee0633057df2087df5dc1268fa30ce
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-07-18 00:21:11 +0530

    create prometheus server

--- main.tf
+++ main.tf
@@ -2,7 +2,14 @@ resource "aws_instance" "ec2_instance" {
     ami           = data.aws_ami.rhel_info.id
     instance_type = var.ec2_instance.instance_type
     vpc_security_group_ids = [var.allow_everything]
-    
+    root_block_device {
+        encrypted             = false
+        volume_type           = "gp3"
+        volume_size           = 100
+        iops                  = 3000
+        throughput            = 125
+        delete_on_termination = true
+    }
     tags = {
         Name = "prometheus_server"
     }
